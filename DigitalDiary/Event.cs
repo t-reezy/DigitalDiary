2bf81467a464f900d896f05c39842c799cfe40bc
Tereza <reezy.kr@gmail.com>
2022-10-25 21:27:34 +0200

    Starting project

---
+++ DigitalDiary/Event.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalDiary
+{
+    internal class Event
+    {
+
+        //public string EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime Date { get; set; }
+
+        public Event(string eventName, DateTime date)
+        {
+            EventName = eventName;
+            Date = date; 
+        }
+        public Event(string eventName, DateTime date, string note, string withWhom)
+        {
+
+        }
+
+
+        public void EditEventName(string newName)
+        {
+            EventName = newName;
+        }
+
+        public void Cancel()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+    }
+}
--- DigitalDiary/Program.cs
+++ DigitalDiary/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DigitalDiary
@@ -10,6 +12,48 @@ namespace DigitalDiary
     {
         static void Main(string[] args)
         {
+            bool run = true;
+
+            Console.WriteLine("My Digital Diary");
+
+
+            while (run)
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1 - Insert new event");
+                Console.WriteLine("2 - Print my plans!");
+                Console.WriteLine("0 - Close the diary");
+                string diaryInput = Console.ReadLine();
+
+                switch (diaryInput)
+                {
+                    case "1":
+                        InsertEvent();
+                        break;
+                    case "2":
+                        PrintMyPlans();
+                        break;
+                    case "0":
+                        run = false;
+                        Console.WriteLine("Closing the diary!");
+                        Thread.Sleep(3000);
+                        break;
+                       
+                }
+                
+            }
+
+
+            
+        }
+        public static void InsertEvent()
+        {
+            Console.WriteLine("Event Inserted");
+        }
+
+        public static void PrintMyPlans()
+        {
+            Console.WriteLine("Those are your plans:");
         }
     }
 }
